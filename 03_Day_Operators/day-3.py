7c7d6144d9abca6a4aa060ec9ab0b5e4a76ce44d
RubenArenhardt <rubenarenhardt2805@outlook.com>
2024-07-08 12:55:57 -0300

    Commit anterior ao envio da garantia do notebook

--- 03_Day_Operators/day-3.py
+++ 03_Day_Operators/day-3.py
@@ -1,6 +1,6 @@
 # Arithmetic Operations in Python
 # Integers
-
+"""
 print('Addition: ', 1 + 2)
 print('Subtraction: ', 2 - 1)
 print('Multiplication: ', 2 * 3)
@@ -122,3 +122,144 @@ print(not True)      # False - Negation, the not operator turns true to false
 print(not False)     # True
 print(not not True)  # True
 print(not not False) # False
+"""
+#Declare your age as integer variable
+
+age = int(20)
+
+#Declare your height as a float variable
+
+height = float(1.86)
+
+#Declare a variable that store a complex number
+
+comp = complex(1 + 2j)
+
+#Write a script that prompts the user to enter base and 
+# height of the triangle and calculate an area of this 
+# triangle (area = 0.5 x b x h).
+
+print('Calculatin the area of a triangle')
+b = float(input('Whats the size of the base?'))
+h = float(input('Whats the size of the height?'))
+print('The triangle has an area of:',0.5 * b * h)
+
+#Write a script that prompts the user to enter side a, 
+# side b, and side c of the triangle. Calculate the 
+# perimeter of the triangle (perimeter = a + b + c).
+
+print('Calculating the perimeter of a triangle')
+side_a = floar(input('Whats the size of the first side?'))
+side_b = floar(input('Whats the size of the second side?'))
+side_c = floar(input('Whats the size of the third side?'))
+print('The perimeter is:',side_a+side_b+side_c)
+
+#Get length and width of a rectangle using prompt. 
+# Calculate its area (area = length x width) and 
+# perimeter (perimeter = 2 x (length + width))
+
+print('Calculatin the area and the perimeter of a rectangle')
+l = float(input('Whats the size of the length?'))
+w = float(input('Whats the size of the width?'))
+print('The rectagle has an area of:',l * w)
+print('The rectangle has a perimeter of:',2*(l+w))
+
+#Get radius of a circle using prompt. Calculate the 
+# area (area = pi x r x r) and circumference 
+# (c = 2 x pi x r) where pi = 3.14.
+
+pi = 13.14
+print('Calculating the area and the circunference of a circle')
+r = float(input('Whats the radius of the circle?'))
+print('The area of the circle is:',pi*r**2)
+print('The perieter of the circle is:',2*pi*r)
+
+#Calculate the slope, x-intercept and y-intercept of y = 2x -2
+
+
+
+#Slope is (m = y2-y1/x2-x1). Find the slope and 
+# Euclidean distance between point (2, 2) and point (6,10)
+
+
+
+#Compare the slopes in tasks 8 and 9.
+
+
+
+#Calculate the value of y (y = x^2 + 6x + 9). 
+# Try to use different x values and figure out at 
+# what x value y is going to be 0.
+
+
+
+#Find the length of 'python' and 'dragon' and make a 
+# falsy comparison statement.
+
+
+
+#Use and operator to check if 'on' is found in both 
+# 'python' and 'dragon'
+
+
+
+#I hope this course is not full of jargon. Use in 
+# operator to check if jargon is in the sentence.
+
+
+
+#There is no 'on' in both dragon and python
+
+
+
+#Find the length of the text python and convert the 
+# value to float and convert it to string
+
+
+
+#Even numbers are divisible by 2 and the remainder is 
+# zero. How do you check if a number is even or not using python?
+
+print('Even or odd')
+num_check = int(input('What is the number?'))
+if num_check % 2 == 0:
+     print('even')
+else:
+     print('odd')
+
+#Check if the floor division of 7 by 3 is equal to 
+# the int converted value of 2.7.
+
+7//3 == int(2.7)
+
+#Check if type of '10' is equal to type of 10
+
+type('10')
+type(10)
+
+#Check if int('9.8') is equal to 10
+
+int(9.8) == 10
+
+#Write a script that prompts the user to enter hours 
+# and rate per hour. Calculate pay of the person?
+
+
+
+#Write a script that prompts the user to enter number 
+# of years. Calculate the number of seconds a person can 
+# live. Assume a person can live hundred years
+
+
+
+#Write a Python script that displays the following table
+
+list_exp = [1, 0, 1, 2, 3]
+list_num = [1,2,3,4,5]
+
+for x in list_num:
+    list_ans=[]
+    for y in list_exp:
+         list_ans.append(x**y)
+    print(list_ans)
+    
